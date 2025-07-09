74be897ca74e0d19c772d3a9412222dca7435dfb
W8jonas <jonashenrique989@gmail.com>
2025-07-09 15:57:14 -0300

    Cria Subject

--- src/main/java/com/simulador/model/domain/Subject.java
+++ src/main/java/com/simulador/model/domain/Subject.java
@@ -1,4 +1,30 @@
 package com.simulador.model.domain;
 
-public class Subject {
+public abstract class Subject {
+    private final String code;
+    private final String name;
+    private final int weeklyHours;
+
+    protected Subject(String code, String name, int weeklyHours) {
+        this.code = code;
+        this.name = name;
+        this.weeklyHours = weeklyHours;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeeklyHours() {
+        return weeklyHours;
+    }
+
+    @Override
+    public String toString() {
+        return code + " - " + name;
+    }
 }
